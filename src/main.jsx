8d91ddb7efed1be09d58e18733a0858a6065f089
gihwan0501 <rlghkskim0501@gmail.com>
2024-10-01 15:16:17 +0900

    #2 컴포넌트 연습 컴포넌트 sample 디렉토리

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+// 가장 최상위의 컴포넌트 App
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App/>
   </StrictMode>,
 )
