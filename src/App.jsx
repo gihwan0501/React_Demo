43e188a95d14508ed38310d5bde01d83399d0133
gihwan0501 <rlghkskim0501@gmail.com>
2024-10-01 16:18:59 +0900

    #3 예약프로젝트 nav Link, BrowserRouter 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
 // 초기 App.jsx
 import './App.css'
-import Garage, {Car} from "./basic/List.jsx";
-import GarageKorean from "./basic/Objects.jsx";
+import {BrowserRouter, Link} from "react-router-dom";
+import {FaCalendarAlt, FaDoorOpen, FaUsers} from "react-icons/fa";
+import UserPicker from "./component/Users/UserPicker.jsx";
+
 // index.html이 처름 요청으로 반환되는 페이지
 // html은 더 만들지 않는다. : SinglePageApplication(SPA)
 // index.html -> main.jsx(index.js) 실행
 function App() {
 
   return (
-    <>  
-        {/*brand 임의의 프로퍼티 설정*/}
-        <Car brand="소나타"/>
-        <Garage/>
-        <GarageKorean/>
-    </>
+      <BrowserRouter>
+            <div className="App">
+                <header>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/bookings" className="btn-header">
+                                    <FaCalendarAlt/>
+                                    <span>예약하기</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/bookables" className="btn-header">
+                                    <FaDoorOpen/>
+                                    <span>예약 자원</span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/users" className="btn-header">
+                                    <FaUsers/>
+                                    <span>사용자</span>
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <UserPicker/>
+                </header>
+            </div>
+      </BrowserRouter>
   )
 }
 
