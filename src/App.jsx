4b8435c6c1bba0d736763302de9d2bccfae25bb2
gihwan0501 <rlghkskim0501@gmail.com>
2024-10-02 16:38:36 +0900

    #10 useEffect userList 컴포넌트는 비동기 통신으로 데이터 수신

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import './App.css'
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import {FaCalendarAlt, FaDoorOpen, FaUsers} from "react-icons/fa";
 import UserPicker from "./component/Users/UserPicker.jsx";
-import React from "react";
 import BookablePage from "./component/Bookables/BookablePage.jsx";
-import UserPages from "./component/Users/UserPages.jsx";
+import UserPage from "./component/Users/UsersPage.jsx";
 import BookingsPage from "./component/Bookings/BookingsPage.jsx";
 
 // index.html이 처름 요청으로 반환되는 페이지
@@ -13,26 +12,26 @@ import BookingsPage from "./component/Bookings/BookingsPage.jsx";
 // index.html -> main.jsx(index.js) 실행
 function App() {
 
-  return (
-      <BrowserRouter>
+    return (
+        <BrowserRouter>
             <div className="App">
                 <header>
                     <nav>
                         <ul>
                             <li>
-                                <Link to="/bookings" className="btn-header">
+                                <Link to="/bookings" className="btn">
                                     <FaCalendarAlt/>
                                     <span>예약하기</span>
                                 </Link>
                             </li>
                             <li>
-                                <Link to="/bookables" className="btn-header">
+                                <Link to="/bookables" className="btn">
                                     <FaDoorOpen/>
                                     <span>예약 자원</span>
                                 </Link>
                             </li>
                             <li>
-                                <Link to="/users" className="btn-header">
+                                <Link to="/users" className="btn">
                                     <FaUsers/>
                                     <span>사용자</span>
                                 </Link>
@@ -45,11 +44,11 @@ function App() {
                 <Routes>
                     <Route path="/bookings" element={<BookingsPage/>}></Route>
                     <Route path="/bookables" element={<BookablePage/>}></Route>
-                    <Route path="/users" element={<UserPages/>}></Route>
+                    <Route path="/users" element={<UserPage/>}></Route>
                 </Routes>
             </div>
-      </BrowserRouter>
-  )
+        </BrowserRouter>
+    )
 }
 
 export default App
---
+++ src/component/Users/UsersPage.jsx
@@ -0,0 +1,12 @@
+import UserList from "./UserList.jsx";
+
+function UsersPage(){
+    return(
+        <main className="users-page">
+            <UserList/>
+
+        </main>
+    )
+}
+
+export default UsersPage
