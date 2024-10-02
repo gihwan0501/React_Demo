c079dbd25da1a8744ba140071d452fe65d4903ce
gihwan0501 <rlghkskim0501@gmail.com>
2024-10-02 10:21:36 +0900

    #4 book페이지

--- src/component/Bookables/BookList.jsx
+++ src/component/Bookables/BookList.jsx
@@ -1,27 +1,81 @@
-import {bookables} from "../../static.json"
+import {bookables, days, sessions} from "../../static.json"
+import {useState} from "react";
+import {FaArrowRight} from "react-icons/fa";
 
 function BookList() {
-    console.log("bookables", bookables)
-    const group = "Rooms"
-    let bookableIndex = 1
-    const bookableGroup = bookables.filter(b => (b.group === group))
 
-    function changeBookable(selectIndex) {
-        bookableIndex = selectIndex
+    const [group, setGroup] = useState("Rooms")
+    const bookableGroup = bookables.filter(b => (b.group === group))
+    // 상태값 관리를 해야 할 변수 bookableIndex
+    // setBookableIndex는 useState가 리턴해주는 메소드. (값 변경 메소드)
+    const [bookableIndex, setBookableIndex] = useState(0)
+    const groups = [...new Set(bookables.map(b=>b.group))]// 현재 상황인거고, 그룹이 추가되면 바꿔야함 ["Rooms", "Kit"]
+    // b.group만 가져와서 컬렉션.중복값은 1개만 저장하기 위해 Set 자료구조
+    // ...은 Set 오브젝트를 배열로 변환
+
+    function nextBookableIndex() {
+        setBookableIndex((i) => (i+1) % bookableGroup.length)
+        // 상태값 변경 메소드의 인자 i는 bookableIndex 값
     }
 
+    function changeGroup(event) {
+        setGroup(event.target.value)
+        setBookableIndex(0)
+    }
+
+    const bookable = bookableGroup[bookableIndex]
+    const [hasDetails, setHasDetails] = useState(false);
     return (
+        <>
+        <div>
+            <select value={group} onChange={changeGroup}>
+                {groups.map(g =><option key={g} value={g}>{g}</option>)}
+            </select>
         <ul className="items-list-nav">
             {bookableGroup.map((b,i) => (
                 <li key={b.id}
                     className={i=== bookableIndex? "selected":null}>
                     <button className="btn"
-                        onClick={() => changeBookable(i)}>
+                        onClick={() => setBookableIndex(i)}>
                         {b.title}
                     </button>
                 </li>
             ))}
         </ul>
+            <p>
+                <button className="btn" onClick={nextBookableIndex}>
+                    <FaArrowRight/><span>Next</span>
+                </button>
+            </p>
+        </div>
+
+        {/* 새로운 UI 추가, CSS 추가 : 상세 내용 */}
+        <div className="book-details">
+            <div className="item">
+                <h2>{bookable.title}</h2>
+                <span className="controls">
+                    <label>
+                        <input type="checkbox" checked={hasDetails} onChange={()=>setHasDetails(has=>!has)}/>
+                    </label>
+                </span>
+            </div>
+            <p>{bookable.notes}</p>
+            {hasDetails && (
+                <div className="item-details">
+                    <h3>사용 가능한 요일과 세션</h3>
+                    <div className="bookable-availability">
+                        <ul>
+                            {bookable.days.sort().map(d => <li key={d}>{days[d]}</li>)}
+                        </ul>
+                        <ul>
+                            {bookable.sessions.sort().map(s => <li key={s}>{sessions[s]}</li>)}
+                        </ul>
+                    </div>
+                </div>
+            )}
+        </div>
+        </>
     )
 }
-export default bookList
+
+export default BookList
