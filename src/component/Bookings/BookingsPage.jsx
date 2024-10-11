6e796e03f77ade3f1ef81208885d8d74936ca957
gihwan0501 <rlghkskim0501@gmail.com>
2024-10-11 10:19:53 +0900

    # 리액트 useQuery bookables

--- src/component/Bookings/BookingsPage.jsx
+++ src/component/Bookings/BookingsPage.jsx
@@ -1,16 +1,19 @@
 import WeekPicker from "./WeekPicker.jsx";
-import BookablesList from "../Bookables/BookablesList.jsx";
-import {useState} from "react";
-import Bookings from "./Bookings.jsx";
 import useFetch from "../utils/useFetch.js";
+import {useState} from "react";
+import BookablesList from "../Bookables/BookablesList.jsx";
+import Bookings from "./Bookings.jsx";
 import {useSearchParams} from "react-router-dom";
 import PageSpinner from "../UI/PageSpinner.jsx";
+import {useQuery} from "react-query";
+import loadData from "../utils/api.js";
 
 function BookingsPage(){
     // 2) fetch로 직접 해당 자원 조회 -> BookablesList 컴포넌트에게 전달
-    const {data:bookables = [], status, error} = useFetch(
-        "http://localhost:3001/bookables"
-    )
+    const {data: bookables = [], status, error} = useQuery(
+        "bookables", () => loadData("http://localhost:3001/bookables")
+    );
+
     // 3) /bookings?bookableId=3과 같이 쿼리스트링값을 가져오기
     // url에 실려오는 쿼리문자열을 가져오기 위해 2개의 값을 배열로 리턴해준다
     // searchParam 첫번째 리턴값은 함수 : 이 함수로 파라미터 가져온다
